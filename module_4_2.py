2a35d597f4d5781f698b3f51938eeda1a55aeada
RenatUzbekov <renat85m@mail.ru>
2024-08-28 23:13:53 +0300

    Add files via upload

---
+++ module_4_2.py
@@ -0,0 +1,9 @@
+def test_function():
+    def inner_function():
+        print("Я в области видимости функции test_function")
+    inner_function()# This is a sample Python script.
+
+inner_function()
+def inner_function():
+   print("Я в области видимости функции test_function")
+inner_function()
